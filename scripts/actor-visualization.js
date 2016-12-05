ac64be50746ec1be7d8e2d9b3e444a11b0785a37
Patrick Niebrzydowski <patrick.niebrzydowski@students.fh-hagenberg.at>
2016-12-05 11:10:46 +0100

    Separate out line drawing and axis drawing

--- scripts/actor-visualization.js
+++ scripts/actor-visualization.js
@@ -1,12 +1,21 @@
 function LineGraph() {
 	this.people = [];
+	this.vis = d3.select('#visualization');
+	this.WIDTH = 1000;
+	this.HEIGHT = 500;
+	this.MARGINS = {
+			top: 20,
+		    right: 20,
+		    bottom: 20,
+		    left: 50
+		};
 	this.lineColors = [
-    	'#ff0000',
-    	'#00ff00',
-    	'#0000ff',
-    	'#0f0f0f',
-    	'#930039'
-    ];
+	    	'#ff0000',
+	    	'#00ff00',
+	    	'#0000ff',
+	    	'#0f0f0f',
+	    	'#930039'
+	    ];
 }
 
 LineGraph.prototype.getDateRange = function getDateRange() {
@@ -31,41 +40,14 @@ LineGraph.prototype.getDateRange = function getDateRange() {
 LineGraph.prototype.drawGraph = function drawGraph() {
 	var self = this;
 	
-	self.drawGraphAxis();
-	//self.drawGraphLines();
+	//self.drawGraphAxis();
+	self.drawGraphLines();
 }
 
 LineGraph.prototype.drawGraphAxis = function drawGraphAxis() {
 	var self = this;
-
-	for(var i=0; i<self.people.length; i++) {
-
-		var person = self.people[i];
 	
-		var vis = d3.select('#visualization'),
-		WIDTH = 1000,
-		HEIGHT = 500,
-		MARGINS = {
-			top: 20,
-		    right: 20,
-		    bottom: 20,
-		    left: 50
-		},
-		
-		xRange = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.min(person.films, function(d) {    
-		    return d.getYear();
-		}), d3.max(person.films, function(d) {  
-		    return d.getYear();
-		})]),
-		
-		yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(person.films, function(d) {
-		    return 0;
-		}), d3.max(person.films, function(d) {
-		    return 10;
-		})]),
-	
-	
-	    xAxis = d3.svg.axis()
+	var xAxis = d3.svg.axis()
 	    .scale(xRange)
 	    .tickSize(2)
 	    .tickSubdivide(true),
@@ -74,31 +56,50 @@ LineGraph.prototype.drawGraphAxis = function drawGraphAxis() {
 	    .tickSize(2)
 	    .orient('left')
 	    .tickSubdivide(true);
+
+    /*vis.append('svg:g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
+    .call(xAxis);
+     */
+    vis.append('svg:g')
+    .attr('class', 'y axis')
+    .attr('transform', 'translate(' + (self.MARGINS.left) + ',0)')
+    .call(yAxis);
+}
+
+LineGraph.prototype.drawGraphLines = function drawGraphLines() {
+	var self = this;
 	
-	    /*vis.append('svg:g')
-	    .attr('class', 'x axis')
-	    .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
-	    .call(xAxis);
-	     */
-	    vis.append('svg:g')
-	    .attr('class', 'y axis')
-	    .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
-	    .call(yAxis);
+	for(var i=0; i<self.people.length; i++) {
+		var person = self.people[i];
+			
+		var 
+			xRange = d3.scale.linear().range([self.MARGINS.left, self.WIDTH - self.MARGINS.right]).domain([d3.min(person.films, function(d) {    
+			    return d.getYear();
+			}), d3.max(person.films, function(d) {  
+			    return d.getYear();
+			})]),
+			
+			yRange = d3.scale.linear().range([self.HEIGHT - self.MARGINS.top, self.MARGINS.bottom]).domain([d3.min(person.films, function(d) {
+			    return 0;
+			}), d3.max(person.films, function(d) {
+			    return 10;
+			})]),	
 	
-	
-	    var lineFunc = d3.svg.line()
-	    .x(function(d) {
-	      return xRange(d.getYear());
-	    })
-	    .y(function(d) {
-	      return yRange(d.vote_average);
-	    })
-	    .interpolate('bundle');
+		    lineFunc = d3.svg.line()
+			    .x(function(d) {
+			      return xRange(d.getYear());
+			    })
+			    .y(function(d) {
+			      return yRange(d.vote_average);
+			    })
+			    .interpolate('bundle');
 	
 	    person.graphLine =
-	    	vis.append('svg:path')
+	    	self.vis.append('svg:path')
 			    .attr('d', lineFunc(person.films))
-			    .attr('stroke', self.lineColors[self..people.length-1])
+			    .attr('stroke', self.lineColors[self.people.length-1])
 			    .attr('stroke-width', 2)
 			    .attr('fill', 'none');
 	}
